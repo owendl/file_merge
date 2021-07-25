f6115e37a96e2eca8f2ed07ca3cf79e57dc359af
Drew Owen <owen.drew.l@gmail.com>
2021-07-25 18:47:45 -0400

    Added first file_merger module

---
+++ file_merger/file_merger.py
@@ -0,0 +1,10 @@
+import os
+import pandas as pd
+import argparse
+
+
+def generic():
+    print("generic function")
+
+if __name__ == "__main__":
+    print("Running file merger")
---
+++ test_script.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 25 18:41:49 2021
+
+@author: drewowen
+"""
+import file_merger.file_merger as fm
+
+fm.generic()
+
