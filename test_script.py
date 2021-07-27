225d05b4e7f3c7237472d72fe44ec7f8746c6eb6
Drew Owen <owen.drew.l@gmail.com>
2021-07-26 21:16:29 -0400

    First pass at cue sheet cleaning

--- test_script.py
+++ test_script.py
@@ -1,11 +1,22 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jul 25 18:41:49 2021
 
-@author: drewowen
-"""
 import file_merger.file_merger as fm
+import pandas as pd
 
 fm.generic()
+
+cue_sheet = "data/TOO LARGE JENNIFER CUE SHEETS 106 MU.xls"
+
+df = pd.read_excel(cue_sheet, skiprows=15)
+
+print(df.head())
+print(len(df))
+
+df.dropna(how="any", inplace=True)
+
+print(len(df))
+new_columns = [x.strip() for x in df.columns.tolist()]
+
+df.columns = new_columns
+print(df.columns.tolist())
+
 
