cfd140d0be458ce3800cdac43611451fbd328d45
Drew Owen <owen.drew.l@gmail.com>
2021-07-27 20:30:53 -0400

    added vendor files dictionary to test_script.py

--- test_script.py
+++ test_script.py
@@ -2,7 +2,16 @@
 import file_merger.file_merger as fm
 import pandas as pd
 
+
+
 fm.generic()
+
+vendor_files = [
+        {"vendor":"FTM","filename": "data/FTM-AA_COMPOSER-PUBLISHER_6-16-21.xlsx", "sheetname":"AA"}
+        ,{"vendor":"AA","filename": "data/FTM-AA_COMPOSER-PUBLISHER_6-16-21.xlsx", "sheetname":"FTM"}
+        ,{"vendor":"SignatureTracks","filename": "data/Signature Tracks - Composer Publisher Info_072621.xlsx"}
+        ,{"vendor":"STKA","filename": "data/STKA_CLIENT_ThruADD86.xlsx"}
+    ]
 
 cue_sheet = "data/TOO LARGE JENNIFER CUE SHEETS 106 MU.xls"
 
