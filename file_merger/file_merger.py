bb1dc86f5219ccdc5d840e3e7d49cd7d7e4de432
Drew Owen <owen.drew.l@gmail.com>
2021-07-27 20:54:56 -0400

    updated description for SignatureTracks formatting function

--- file_merger/file_merger.py
+++ file_merger/file_merger.py
@@ -57,6 +57,13 @@ def format_FTM(df):
 def format_SignatureTracks(df):
     '''
     TODO
+    function for custom processing of Signature Tracks formatted vendor excel file
+
+    input: pandas DataFrame with the following columns: [Title	Publisher 1 Company	Publisher 1 Pro Affiliation	Publisher 1 CAE/IPI	Publisher 1 Ownership Share	Publisher 1 Role	Publisher 1 Collection Share Percentage 1	Publisher 1 Collection Share Territory 1	Publisher 2 Company	Publisher 2 Pro Affiliation	Publisher 2 CAE/IPI	Publisher 2 Ownership Share	Publisher 2 Role	Publisher 2 Collection Share Percentage 1	Publisher 2 Collection Share Territory 1	Publisher 3 Company	Publisher 3 Pro Affiliation	Publisher 3 CAE/IPI	Publisher 3 Ownership Share	Publisher 3 Role	Publisher 3 Collection Share Percentage 1	Publisher 3 Collection Share Territory 1	Publisher 4 Company	Publisher 4 Pro Affiliation	Publisher 4 CAE/IPI	Publisher 4 Ownership Share	Publisher 4 Role	Publisher 4 Collection Share Percentage 1	Publisher 4 Collection Share Territory 1	Publisher 5 Company	Publisher 5 Pro Affiliation	Publisher 5 CAE/IPI	Publisher 5 Ownership Share	Publisher 5 Role	Publisher 5 Collection Share Percentage 1	Publisher 5 Collection Share Territory 1	Writer 1 First Name	Writer 1 Last Name	Writer 1 Company	Writer 1 Pro Affiliation	Writer 1 CAE/IPI	Writer 1 Ownership Share	Writer 1 Publishing Interest	Writer 1 Role	Writer 2 First Name	Writer 2 Last Name	Writer 2 Company	Writer 2 Pro Affiliation	Writer 2 CAE/IPI	Writer 2 Ownership Share	Writer 2 Publishing Interest	Writer 2 Role	Writer 3 First Name	Writer 3 Last Name	Writer 3 Company	Writer 3 Pro Affiliation	Writer 3 CAE/IPI	Writer 3 Ownership Share	Writer 3 Publishing Interest	Writer 3 Role	Writer 4 First Name	Writer 4 Last Name	Writer 4 Company	Writer 4 Pro Affiliation	Writer 4 CAE/IPI	Writer 4 Ownership Share	Writer 4 Publishing Interest	Writer 4 Role	Writer 5 First Name	Writer 5 Last Name	Writer 5 Company	Writer 5 Pro Affiliation	Writer 5 CAE/IPI	Writer 5 Ownership Share	Writer 5 Publishing Interest	Writer 5 Role	Writer 6 First Name	Writer 6 Last Name	Writer 6 Company	Writer 6 Pro Affiliation	Writer 6 CAE/IPI	Writer 6 Ownership Share	Writer 6 Publishing Interest	Writer 6 Role	Writer 7 First Name	Writer 7 Last Name	Writer 7 Company	Writer 7 Pro Affiliation	Writer 7 CAE/IPI	Writer 7 Ownership Share	Writer 7 Publishing Interest	Writer 7 Role]
+
+    output: pandas DataFrame with columns specified by final_columns
+        ex. ["File Name", "Song Name", "Library", "Composer","Publisher","Catalogue Number"]
+    
     '''
     return df
 
